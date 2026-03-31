3fae6b5bdbc293dc1eb84020d7dcccf79f7aed8a
Skullnietz <al221910878@gmail.com>
2026-03-31 14:11:16 -0600

    Actualizacion clientes

--- ServerSide/consultaordenes.php
+++ ServerSide/consultaordenes.php
@@ -1,7 +1,5 @@
 <?php
 include_once '../ServerSide/serversideConexion.php';
-include_once '../config/Database.php';
-include_once '../config/env.php';
 
 $objeto = new ConexionO();
 $conexiono = $objeto->Conectar();
@@ -14,12 +12,18 @@ $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
 // Traer nombres de clientes desde la otra BD
 $clienteNames = [];
 try {
-    $pdoSis = Database::conectar(Database::SISTEMA);
+    $host   = getenv('DB_SISTEMA_HOST') ?: 'localhost';
+    $dbname = getenv('DB_SISTEMA_NAME') ?: 'egsequip_dbsistema';
+    $user   = getenv('DB_SISTEMA_USER') ?: 'egsequip_sistema';
+    $pass   = getenv('DB_SISTEMA_PASS') ?: '{#k%ER.PJD0?';
+    $pdoSis = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']);
     $stmtCli = $pdoSis->prepare("SELECT id, nombre FROM clientesTienda");
     $stmtCli->execute();
     foreach ($stmtCli->fetchAll(PDO::FETCH_ASSOC) as $cli) {
         $clienteNames[intval($cli["id"])] = $cli["nombre"];
     }
+    $pdoSis = null;
 } catch (Exception $e) {}
 
 foreach ($data as &$row) {
